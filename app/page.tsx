57255f3393d39e11f121e7037f599b57f5c813f3
krankos <bessaadkhalil@gmail.com>
2025-03-18 15:01:43 +0100

    improved message

--- app/page.tsx
+++ app/page.tsx
@@ -27,6 +27,8 @@ function AssistantContent() {
       {message with different tone 1} - **{tone 1}**
 
       {message with different tone 2}- **{tone 2}**
+
+      {improved message making it more clear} - **improved**
      ...
     `,
   });
